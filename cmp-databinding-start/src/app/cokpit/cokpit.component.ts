6960ff7695e858722bd422b6c50b8c6d22752694
Andreas Haskaris <andrchas@gmail.com>
2019-09-11 17:47:49 +0300

    Section 5 lesson 67, Custom Event Binding

---
+++ cmp-databinding-start/src/app/cokpit/cokpit.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-cokpit',
+  templateUrl: './cokpit.component.html',
+  styleUrls: ['./cokpit.component.css']
+})
+export class CokpitComponent implements OnInit {
+  @Output() serverCreated     = new EventEmitter<{ serverName:string,serverContent:string }>()
+  @Output() blueprintCreated  = new EventEmitter<{ serverName:string,serverContent:string }>()
+  newServerName               = '';
+  newServerContent            = '';
+
+  constructor() { }
+
+  ngOnInit() {}
+
+  onAddServer() {
+    this.serverCreated.emit({ 
+      serverName: this.newServerName,
+      serverContent : this.newServerContent 
+    })
+  }
+
+  onAddBlueprint() {
+   this.blueprintCreated.emit({
+    serverName: this.newServerName,
+    serverContent : this.newServerContent 
+   })
+  }
+
+}
