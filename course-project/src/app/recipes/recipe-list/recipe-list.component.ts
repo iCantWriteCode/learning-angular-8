f00cb66886b3c13c55490a48dea46d4c1cb1180c
ICantWriteCode <andrchas@gmail.com>
2019-09-14 13:33:17 +0300

    Section 10 Complete

--- course-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,24 +1,23 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
+
 export class RecipeListComponent implements OnInit {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>()
 
-  recipes: Recipe[] = [
-    new Recipe('A test recipe','This is a test', 'https://www.gimmesomeoven.com/wp-content/uploads/2014/03/Cajun-Jambalaya-Recipe-with-Andouille-Sausage-Shrimp-and-Chicken-3-1.jpg')
-  ];
+  recipes: Recipe[]
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
-  ngOnInit() {}
-
-  onRecipeSelected(recipe: Recipe) {
-    this.recipeWasSelected.emit(recipe)
+  ngOnInit() {
+    this.recipes = this.recipeService.getRecipes()
   }
+
+  
 
 }
---
+++ course-project/src/app/recipes/recipe.service.ts
@@ -0,0 +1,42 @@
+import { EventEmitter, Injectable } from '@angular/core'
+
+import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+
+@Injectable()
+export class RecipeService {
+    
+    recipeSelected = new EventEmitter<Recipe>();
+    
+    private recipes: Recipe[] = [
+        new Recipe(
+            'A test recipe',
+            'This is a test', 
+            'https://www.gimmesomeoven.com/wp-content/uploads/2014/03/Cajun-Jambalaya-Recipe-with-Andouille-Sausage-Shrimp-and-Chicken-3-1.jpg',
+            [
+                new Ingredient('Meet', 1),
+                new Ingredient('French Fries', 25)
+            ]
+        ),
+        new Recipe(
+            'A test recipe 2',
+            'This is a test 2', 
+            'https://www.gimmesomeoven.com/wp-content/uploads/2014/03/Cajun-Jambalaya-Recipe-with-Andouille-Sausage-Shrimp-and-Chicken-3-1.jpg',
+            [
+                new Ingredient('Apples', 1),
+                new Ingredient('Buns', 2)
+            ]
+        )
+      ];
+    
+    constructor(private shoppingListService: ShoppingListService) {}
+
+    getRecipes() {
+        return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.shoppingListService.addIngredients(ingredients);
+    }
+}
